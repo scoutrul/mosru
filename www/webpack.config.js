9be23eee46ecebf485ba072a80beea97fdbb64cc
unknown <scoutrul@mail.ru>
2017-03-06 21:07:37 +0300

    v1 chrome ok

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,50 +1,43 @@
 'use strict';
 
+var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
-
+var extractStyles = new ExtractTextPlugin("styles.css");
 
 module.exports = {
     entry: './app/index',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-    },
-
+    
     module: {
       loaders: [{
         test: /\.html$/,
         loader: "raw-loader" // loaders: ['raw-loader'] is also perfectly acceptable.
       }],
 
-      rules:[
-        {
-          test: /\.styl$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader', 'stylus-loader?resolve url']
-          })
-        },
-        // {test: /\.styl$/, loader: ['style-loader!css-loader!stylus-loader?resolve url']},
-        {
-          test: /\.pug$/,
-          loader: 'pug-loader'
-        }
-      ]
-    },
+      rules: [{
+               test: /\.styl$/,
+               use: extractStyles.extract({
+                     fallback: 'postcss-loader',
+                     use: ['css-loader?importLoaders=1!postcss-loader', 'stylus-loader?resolve url']
+                 })
+             },
+             {
+               test: /\.pug$/,
+               loader: 'pug-loader'
+             }
+             
+      ]},
     plugins: [
-        new ExtractTextPlugin("styles.css"),
+        extractStyles,
 
         new HtmlWebpackPlugin({ 
           filename: 'index.html',
           template: './app/index.pug'
-        })
+        }),
+        require('autoprefixer')
     ],
+
     watch: true,
-    watchOptions: {
-      aggregateTimeout: 100
-    },
 
     // devtool: 'source-map',
 
@@ -53,4 +46,10 @@ module.exports = {
       hot: true,
       inline: true
     }
-};
+
+
+    ,output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'public')
+    }
+}
