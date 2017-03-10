2ca06216e1371dd9ddfbf2d641faa6c077112092
unknown <scoutrul@mail.ru>
2017-03-10 06:24:36 +0300

    +

--- www/app/script.js
+++ www/app/script.js
@@ -1,16 +1,35 @@
 import './stylus.styl';
 
-	document.getElementById('list').onchange =  function(){
-			this.className = 'placeholder_in';
-		};
+document.getElementById('photo').onchange =  function(){
+		file_src.innerHTML = this.value;
+	};
+
+document.getElementById('list').onchange =  function(){
+		this.className = 'placeholder_in';
+	};
 
 
-	document.getElementById('tel').onchange =  function(){
-			this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
-		};
+document.getElementById('tel').onchange =  function(){
+		this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
+	};
+document.getElementById('fio').onchange =  function(){
+		this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
+	};
+document.getElementById('pass').onchange =  function(){
+		this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
+	};
+document.getElementById('email').onchange =  function(){
+		this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
+	};
 
 
-	document.getElementById('photo').onchange =  function(){
-			file_src.innerHTML = this.value;
-		};
+// var some = {
+// 	input: ["photo", "list", "tel", "fio", "pass", "email"] 
+// 	method: ["onchange", "onblur"]
+// // }
+
+// var test = function(e){
+// 	var e = document.getElementById(e);
+// 	console.log(e)
+// };
 
