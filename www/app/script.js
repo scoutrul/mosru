20d0308472b58e38b9361bcd3a109b92c48e3dd0
unknown <scoutrul@mail.ru>
2017-03-10 07:48:19 +0300

    check

--- www/app/script.js
+++ www/app/script.js
@@ -5,31 +5,6 @@ document.getElementById('photo').onchange =  function(){
 	};
 
 
-
-// document.getElementById('tel').onchange =  function(){
-// 		this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
-// 	};
-// document.getElementById('fio').onchange =  function(){
-// 		this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
-// 	};
-// document.getElementById('pass').onchange =  function(){
-// 		this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
-// 	};
-// document.getElementById('email').onchange =  function(){
-// 		this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
-// 	};
-
-
-// var some = {
-// 	input: ["photo", "list", "tel", "fio", "pass", "email"] 
-// 	method: ["onchange", "onblur"]
-// // }
-
-// var test = function(e){
-// 	var e = document.getElementById(e);
-// 	console.log(e)
-// };
-
 var inputs = document.querySelectorAll('.test');
 
 for (var i = 0; i < inputs.length; i++) {
