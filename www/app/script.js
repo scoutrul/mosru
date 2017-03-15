4c034a1c8e23bf2472dfe9c3d96f2268cff80859
unknown <scoutrul@mail.ru>
2017-03-15 16:51:07 +0300

    password shower and svg icons

--- www/app/script.js
+++ www/app/script.js
@@ -4,11 +4,10 @@ document.getElementById('photo').onchange =  function(){
 		file_src.innerHTML = this.value;
 	};
 
-
-var inputs = document.querySelectorAll('.test');
+var inputs = document.querySelectorAll('.input');
 
 for (var i = 0; i < inputs.length; i++) {
-  var here = inputs[i];
+	var here = inputs[i];
 
 	here.onclick  = function() {
 		this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
@@ -18,3 +17,22 @@ for (var i = 0; i < inputs.length; i++) {
 		this.value !== '' ? this.className='placeholder_in' : this.className='placeholder_out';
 	};
 }
+
+var passInput = document.getElementById('pass');
+var showPass = false;
+
+document.getElementById('showPass').onclick = function(){
+
+	if (showPass){
+		passInput.style.backgroundImage = "url('/img/eye-a.svg')";
+		passInput.type = "password";
+	}
+	else {
+		passInput.style.backgroundImage = "url('/img/eye-b.svg')";
+		passInput.type = "text";
+	}
+
+
+	showPass = !showPass;
+	console.log(showPass)
+};
